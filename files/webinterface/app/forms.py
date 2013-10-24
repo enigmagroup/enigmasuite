d1fe5a5a31ceca3db9ddf860ae78fe2d6dd63224
Agent 42 <42@enigmabox.net>
2013-10-24 18:22:09 +0200

    need to find out how to skip unique validation on edits

--- files/webinterface/app/forms.py
+++ files/webinterface/app/forms.py
@@ -4,23 +4,31 @@ from django.core import exceptions
 from django.core.validators import validate_slug, validate_ipv6_address
 from app.models import *
 
-def validate_hostname(value):
-    if re.search("[^a-z0-9-]", value):
-        raise exceptions.ValidationError('Der Hostname darf nur aus Kleinbuchstaben, Zahlen und Bindestrichen (-) bestehen.')
-
-def validate_unique_hostname(value):
-    if Address.objects.filter(name=value):
-        raise exceptions.ValidationError('Dieser Hostname ist schon vergeben.')
-
-def validate_unique_ipv6(value):
-    if Address.objects.filter(ipv6=value):
-        raise exceptions.ValidationError('Diese IPv6-Adresse ist schon vergeben.')
-
-def validate_unique_phone(value):
-    if Address.objects.filter(phone=value):
-        raise exceptions.ValidationError('Diese Telefonnummer ist schon vergeben.')
-
 class AddressbookForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(AddressbookForm, self).__init__(*args, **kwargs)
+        print self.changed_data
+
+    def validate_hostname(value):
+        if re.search("[^a-z0-9-]", value):
+            raise exceptions.ValidationError('Der Hostname darf nur aus Kleinbuchstaben, Zahlen und Bindestrichen (-) bestehen.')
+
+# todo: find out how to bypass this on edits
+    def validate_unique_hostname(value):
+        return True
+        if Address.objects.filter(name=value):
+            raise exceptions.ValidationError('Dieser Hostname ist schon vergeben.')
+
+    def validate_unique_ipv6(value):
+        return True
+        if Address.objects.filter(ipv6=value):
+            raise exceptions.ValidationError('Diese IPv6-Adresse ist schon vergeben.')
+
+    def validate_unique_phone(value):
+        return True
+        if Address.objects.filter(phone=value):
+            raise exceptions.ValidationError('Diese Telefonnummer ist schon vergeben.')
+
     name = forms.CharField(initial='', required=True, min_length=1, max_length=60, validators=[validate_hostname, validate_unique_hostname])
     ipv6 = forms.CharField(initial='', validators=[validate_ipv6_address, validate_unique_ipv6])
     phone = forms.IntegerField(initial='', min_value=10, required=False, validators=[validate_unique_phone])
