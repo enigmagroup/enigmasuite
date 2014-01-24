8e11ae1ee91e0814332f6a60f73fd3313a59d27a
Agent 42 <42@enigmabox.net>
2014-01-24 13:34:53 +0100

    stop puppet agent in housekeeping

--- files/housekeeping.sh
+++ files/housekeeping.sh
@@ -4,6 +4,11 @@
 # in case of: "dpkg was interrupted, you must manually run 'dpkg --configure -a' to correct the problem"
 /usr/bin/dpkg --configure -a
 
+# flush logs
+> /var/log/asterisk/messages
+> /var/log/nginx/access.log
+> /var/log/nginx/error.log
+
 # restart puppet, it happened sometimes that "a puppet run is already in progress". Fuck you, memory leaking rubycrap!
 /etc/init.d/puppet stop
 sleep 1
@@ -12,5 +17,9 @@ sleep 3
 killall -9 puppet
 /usr/bin/puppet agent --enable  # remove any lockfiles
 rm -r /var/lib/puppet/*     # give us some memory back
-/etc/init.d/puppet restart
+
+# run puppet, but don't start agent anymore
+rand=$(($RANDOM % 600))
+sleep "$rand"
+/usr/bin/puppet agent --test
 
