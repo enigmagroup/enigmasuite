70f0274fc6a3792ff5a9aad29236246a708090a7
Agent 42 <42@enigmabox.net>
2014-10-03 12:16:37 +0200

    jäjäjä

--- files/puppet/modules/asterisk/manifests/init.pp
+++ files/puppet/modules/asterisk/manifests/init.pp
@@ -1,4 +1,4 @@
-class asterisk($addresses = '') {
+class asterisk($addresses = '', $global_addresses = '') {
 
     Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin" ] }
 
